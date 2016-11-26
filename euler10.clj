83fe7b6129dba12ecf462d42894e5bc6bf06dbe2
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-11-26 05:32:12 +0200

    Complete Euler 10

---
+++ euler10.clj
@@ -0,0 +1,11 @@
+(ns euler10)
+
+(defn prime? [n]
+  (if (> 1 n)
+    false
+    (every? #(not= 0 (mod n %)) (range 2 (inc (int (Math/sqrt n)))))))
+
+(->> (iterate inc 2)
+     (filter prime?)
+     (take-while #(< % 2000000))
+     (reduce +))
