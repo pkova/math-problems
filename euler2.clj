959b6988c68dc8e41d2aae2f4e884b3df327861b
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-11-27 11:08:23 +0200

    Refactor Euler 2 to partial

--- euler2.clj
+++ euler2.clj
@@ -2,4 +2,4 @@
 
 (def fib (map first (iterate (fn [[a b]] [b (+' a b)]) [0 1])))
 
-(reduce + (filter even? (take-while #(< % 4000000) fib)))
+(reduce + (filter even? (take-while (partial > 4000000) fib)))
