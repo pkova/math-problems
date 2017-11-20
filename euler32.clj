4d2c6d098e3fcffe0dac38c9961b12b5f665f2d3
pkova <pyry.kovanen@gmail.com>
2017-11-20 22:43:36 +0200

    Remove unused stuff from euler 32

--- euler32.clj
+++ euler32.clj
@@ -1,5 +1,4 @@
-(ns euler31
-  (:require [clojure.string :as str]))
+(ns euler31)
 
 (defn pandigital? [i n x]
   (let [numbers (set (map str (range 1 10)))]
@@ -7,19 +6,6 @@
       (= x (* i n))
       (= numbers (set (map str (str i n x)))))))
 
-(defn pandigital-2? [n]
-  (let [numbers #{"1" "2" "3" "4" "5" "6" "7" "8" "9"}]
-    (= numbers (set (map str (str n))))))
-
-(defn ok-number-base? [n & r]
-  (let [s (map str (str n))
-        str-set (set s)
-        prev-set (set (map str (apply str r)))]
-    (if (or (contains? str-set "0") (not (empty? (clojure.set/intersection str-set prev-set))))
-      false
-      (>= (count (set s)) (count s)))))
-
-(def ok-number? (memoize ok-number-base?))
 
 (defn factorize [n]
   (let [one-and-four (for [i (range 1 9)
@@ -32,4 +18,4 @@
                        [i j n])]
     (filter #(apply pandigital? %) (concat one-and-four two-and-three))))
 
-(def lel (doall (pmap factorize (range 1000 10000))))
+(reduce + (map (comp last first) (remove empty? (doall (pmap factorize (range 1000 10000))))))
